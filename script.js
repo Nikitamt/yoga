2bc3686d8002bacc88a0b198ad14b4b2f0acf9df
Nikitamt <ntarasov0326@icloud.com>
2024-01-22 16:04:12 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
+  'use strict'
 
   let info = document.querySelector('.info-header'),
       tabContent = document.querySelectorAll('.info-tabcontent'),
